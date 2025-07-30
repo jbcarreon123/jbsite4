18ac75c1db0f66b821b31e3aa7117ce69c086348
Jb Carreon <jbcarreon212@gmail.com>
2025-07-30 11:54:08 +0800

    sbr update

--- src/pages/imgs/site-buttons/[slug].ts
+++ src/pages/imgs/site-buttons/[slug].ts
@@ -10,13 +10,13 @@ export function getStaticPaths() {
     buttons = Buttons
         .filter((btn) => !btn.imgUrl.startsWith('/imgs'))
         .map((btn) => {
-            let url = new URL(btn.imgUrl)
+            let dom = new URL(btn.url);
+            let url = new URL(btn.imgUrl);
             let spl = url.pathname.split('/')
             let ext = spl[spl.length - 1]
-            let nme = url.hostname.replace('www', '').split('.')[0]
             let fex = ext.split('.')
 
-            return { slug: `${nme}-${ext}`, url: btn.imgUrl, format: fex[fex.length - 1] }
+            return { slug: `${dom.hostname}.${fex[fex.length - 1]}`, url: btn.imgUrl, format: fex[fex.length - 1] }
         });
 
     let btns = buttons.map((btn) => ({
