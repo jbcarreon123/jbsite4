0735f9081c0c74dae75b1dc07a3068b6d64d6b39
Jb Carreon <jbcarreon212@gmail.com>
2025-09-13 02:00:00 +0800

    modify some stuff

--- src/components/Stats.svelte
+++ src/components/Stats.svelte
@@ -10,43 +10,89 @@
             followers: json.followers
         }
     }
+
+    async function loadNeoStats() {
+        const request = await fetch("https://corsproxy.io/?https://neocities.org/api/info?sitename=jbcarreon123");
+        let json = await request.json();
+
+        return {
+            hits: json.info.hits,
+            views: json.info.views,
+            updated: new Date(json.info.last_updated).toLocaleDateString(),
+            created: new Date(json.info.created_at).toLocaleDateString()
+        }
+    }
 </script>
 
-<div>
-    {#await loadStats()}
-        <p>Loading stats...</p>
-    {:then out}
-    <table>
-        <tbody>
-            <tr>
-                <td><b>Created:</b></td>
-                <td>{out.created}</td>
-            </tr>
-            <tr>
-                <td><b>Updated:</b></td>
-                <td>{out.updated}</td>
-            </tr>
-            <tr>
-                <td><b>Views:</b></td>
-                <td>{out.views}</td>
-            </tr>
-            <tr>
-                <td><b>Followers:</b></td>
-                <td>{out.followers}</td>
-            </tr>
-        </tbody>
-    </table>
-    {:catch err}
-        <p>Error occured. {err}</p>
-    {/await}
+<div class="stats-container">
+    <div class="nkw-stats">
+        <p class="tg">Nekoweb Stats</p>
+        {#await loadStats()}
+            <p>Loading stats...</p>
+        {:then out}
+        <table>
+            <tbody>
+                <tr>
+                    <td><b>Created:</b></td>
+                    <td>{out.created}</td>
+                </tr>
+                <tr>
+                    <td><b>Updated:</b></td>
+                    <td>{out.updated}</td>
+                </tr>
+                <tr>
+                    <td><b>Views:</b></td>
+                    <td>{out.views}</td>
+                </tr>
+                <tr>
+                    <td><b>Followers:</b></td>
+                    <td>{out.followers}</td>
+                </tr>
+            </tbody>
+        </table>
+        {:catch err}
+            <p>Error occured. {err}</p>
+        {/await}
+    </div>
+    <div class="neo-stats">
+        <p class="tg">Neocities Stats</p>
+        {#await loadNeoStats()}
+            <p>Loading stats...</p>
+        {:then out}
+        <table>
+            <tbody>
+                <tr>
+                    <td><b>Created:</b></td>
+                    <td>{out.created}</td>
+                </tr>
+                <tr>
+                    <td><b>Updated:</b></td>
+                    <td>{out.updated}</td>
+                </tr>
+                <tr>
+                    <td><b>Views:</b></td>
+                    <td>{out.views}</td>
+                </tr>
+                <tr>
+                    <td><b>Hits:</b></td>
+                    <td>{out.hits}</td>
+                </tr>
+            </tbody>
+        </table>
+        {:catch err}
+            <p>Error occured. {err}</p>
+        {/await}
+    </div>
 </div>
 
 <style>
     tr td:first-child {
-        width: 100px;
+        width: 90px;
     }
 
-    tr td:last-child {
-        padding-left: 8px;
+    .stats-container {
+        display: grid;
+        grid-template-columns: 1fr 1fr;
+        width: 100%;
     }
 </style>
