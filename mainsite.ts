1f2db6bab113cab1b34b7fc4010e2a99326980b0
Jb Carreon <jbcarreon212@gmail.com>
2025-09-13 02:11:06 +0800

    try again

--- mainsite.ts
+++ mainsite.ts
@@ -7,8 +7,8 @@ const entries = fsWalk.walkSync('./dist');
 entries.forEach(async e => {
     if (e.name.endsWith('.html')) {
         const htmlContent = readFileSync(e.path, 'utf8');
-        let minifiedHTML = htmlContent.replaceAll('data-main-site="true" href="', 'data-main-site="true" href="https://jbc.lol');
-        minifiedHTML = minifiedHTML.replaceAll('data-gh-site="true" src="', 'data-gh-site="true" src="https://gh.jbc.lol');
+        let minifiedHTML = htmlContent.replace(/href="([\/\.\-\_:\w]+")(.+data-main-site="true")/gm, 'href="https://jbc.lol$1$2');
+        minifiedHTML = minifiedHTML.replace(/href="([\/\.\-\_:\w]+")(.+data-gh-site="true")/gm, 'href="https://gh.jbc.lol$1$2');
         writeFileSync(e.path, minifiedHTML, 'utf8');
         console.log(`Converted urls for ${e.path}`);
     }
