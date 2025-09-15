e7ea6ccad91e9618329b4b4fe9a33fa7c9d4cd73
jbcarreon123 <jbcarreon212@gmail.com>
2025-09-15 23:11:49 +0800

    build stuff

--- src/lib/loadMusic.ts
+++ src/lib/loadMusic.ts
@@ -2,6 +2,6 @@ import { readdirSync } from 'node:fs';
 import path from 'node:path';
 
 export async function loadMusic() {
-    let files = readdirSync('./public/music').filter((v) => v.endsWith('mp3') || v.endsWith('it') || v.endsWith('mod'));
+    let files = readdirSync('./public/music').filter((v) => v.endsWith('mp3') || v.endsWith('m4a') || v.endsWith('it') || v.endsWith('mod'));
     return files
 }
