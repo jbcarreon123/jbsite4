97e6012ae85dde08801f3b927a9c814592319ab4
jbcarreon123 <jbcarreon212@gmail.com>
2025-09-15 23:33:37 +0800

    change stuff

--- mainsite.ts
+++ mainsite.ts
@@ -8,7 +8,7 @@ entries.forEach(async e => {
     if (e.name.endsWith('.html') || e.name.endsWith('.css')) {
         const htmlContent = readFileSync(e.path, 'utf8');
         let minifiedHTML = htmlContent.replace(/href=([\/\.\-\_:\w]+)([\w =-]+data-main-site=true)/g, 'href=https://jbc.lol$1$2');
-        minifiedHTML = minifiedHTML.replace(/href=([\/\.\-\_:\w]+)([\w =-]+data-gh-site=true)/g, 'href=https://gh.jbc.lol$1$2');
+        minifiedHTML = minifiedHTML.replace('/music/', 'https://gh.jbc.lol/music/');
         minifiedHTML = minifiedHTML.replaceAll("#REMOVE_THIS::", '::');
         if (htmlContent !== minifiedHTML) {
             writeFileSync(e.path, minifiedHTML, 'utf8');
