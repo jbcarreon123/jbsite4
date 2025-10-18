0e591ba9e9b57feabb063115b9f0101e19cbc4c8
jbcarreon123 <jbcarreon212@gmail.com>
2025-10-18 23:21:00 +0800

    fix stuff

--- src/components/Nekomment.svelte
+++ src/components/Nekomment.svelte
@@ -1,54 +1,141 @@
 <script lang="ts">
-    export let url;
     import { onMount } from "svelte";
+    import MarkdownIt from "markdown-it";
 
-    let comments = [];
+    const processor = new MarkdownIt();
+
+    const { path } = $props();
+    let comments: any[] = $state([]);
 
     onMount(async () => {
         try {
             const request = await fetch(
-                `https://cmt.nkko.link/api/maxpixels.moe/${url}`,
+                `https://cmt.nkko.link/api/jbc.lol${path}`,
             );
             const json = await request.json();
 
             console.log(json);
 
-            comments = json;
+            comments = json.sort((a, b) => { return new Date(b.createdAt) - new Date(a.createdAt) });
         } catch (error) {
             console.error("there was an attempt. " + error);
         }
     });
+
+    let parentId = $state("");
+    let name = $state("");
+    let cbox: HTMLFormElement | undefined = $state();
 </script>
 
-<div class="comments">
+<div class="c_widget">
+    <form class="nkm-editor" method="POST" action="https://cmt.nkko.link/api/jbc.lol{path}" bind:this={cbox}>
+        <div class="nkm-topInput">
+            <input type="text" maxlength="64" name="name" placeholder="Display name" aria-label="Display name" required />
+            <input type="url" maxlength="64" name="website" placeholder="Website (optional)" aria-label="Website (optional)" />
+        </div>
+        <textarea name="content" maxlength="1024" placeholder="Your comment..." aria-label="Your comment" required></textarea>
+        {#if parentId}<input type="hidden" id="parentid" name="parentId" value={parentId}>{/if}
+        <div class="bottom">
+            {#if !!parentId}
+            <p><span aria-hidden="true" class="ms" data-icon="reply"></span> Replying to {name}</p>
+            <button class="nkm-button" onclick={(e)=>{e.preventDefault();parentId="";name=""}}><span aria-hidden="true" class="ms" data-icon="cancel"></span> Cancel Reply</button>
+            {/if}
+            <button class="nkm-button nkm-comment"><span aria-hidden="true" class="ms" data-icon="send"></span> Send!</button>
+        </div>
+    </form>
     {#each comments as comment}
         <div id="comment">
-            <span>
-                <h2>{comment.author}</h2>
-                <a href={comment.website}>{comment.website}</a>
-                <p>{new Date(comment.createdAt).toLocaleDateString()}</p>
-            </span>
-            <p>{comment.content}</p>
+            <div class="info">
+                <h3>{comment.author}</h3>
+                <span>
+                    {#if comment.website}<p><a href={comment.website} target="_blank">{new URL(comment.website).hostname}</a></p>{/if}
+                    <p class="date">
+                        {new Date(comment.createdAt).toLocaleDateString()}
+                    </p>
+                </span>
+            </div>
+            <p>{processor.renderInline(comment.content)}</p>
+            <button onclick={(e)=>{e.preventDefault();parentId=comment.id;name=comment.author;cbox?.scrollIntoView()}}><span aria-hidden="true" class="ms" data-icon="reply"></span> Reply</button>
             {#if comment.replies}
                 <div class="replies">
                     {#each comment.replies as reply}
                         <div id="reply">
-                            <span>
-                                <h2>{reply.author}</h2>
-                                <a href={comment.website}>{reply.website}</a>
-                                <p>
-                                    {new Date(
-                                        reply.createdAt,
-                                    ).toLocaleDateString()}
-                                </p>
-                            </span>
-                            <p>{reply.content}</p>
+                            <div class="info">
+                                <h3>{reply.author}</h3>
+                                <span
+                                    >{#if reply.website}<p><a href={reply.website} target="_blank">{new URL(reply.website).hostname}</a></p>{/if}
+                                    <p class="date">
+                                        {new Date(
+                                            reply.createdAt,
+                                        ).toLocaleDateString()}
+                                    </p></span
+                                >
+                            </div>
+                            <p>{@html processor.renderInline(reply.content)}</p>
                         </div>
                     {/each}
                 </div>
             {/if}
         </div>
     {:else}
-        <h2 style="text-align:center">Loading..</h2>
+        <p>Loading comments...</p>
     {/each}
 </div>
+
+<style scoped>
+    .c_widget {
+        display: flex;
+        flex-direction: column;
+        gap: 6px;
+        #comment {
+            background-color: var(--altbg);
+            padding: 8px;
+
+            .info {
+
+                display: flex;
+                justify-content: space-between;
+
+                h3, p {
+                    padding: 0;
+                    margin: 0;
+                }
+
+                span {
+                    display: flex;
+                    gap: 6px;
+                }
+            }
+
+            #reply {
+                background-color: var(--acc);
+                margin: 6px;
+                margin-left: 24px;
+                padding: 8px;
+                .info {
+                    background-color: var;
+                }
+            }
+        }
+
+        .nkm-editor {
+            background-color: var(--altbg);
+            padding: 8px;
+            display: flex;
+            flex-direction: column;
+            gap: 6px;
+
+            .nkm-topInput {
+                display: grid;
+                grid-template-columns: 1fr 1fr;
+                gap: 6px;
+            }
+
+            .bottom {
+                display: flex;
+                justify-content: end;
+                gap: 6px;
+            }
+        }
+    }
+</style>
